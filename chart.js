de2ae8998636451cf5f2f4aad3d373a32c6ecee7
Aryan Anand <aryanaa2005@gmail.com>
2025-10-16 00:06:18 +0530

    Chart added to show rating graph

---
+++ chart.js
@@ -0,0 +1,70 @@
+const ctx = document.getElementById("myLineChart");
+
+function TimeFormatter(contestTimestampSeconds) {
+  const contestTimestampMilliseconds = contestTimestampSeconds * 1000;
+  const dateObject = new Date(contestTimestampMilliseconds);
+  const year = dateObject.getFullYear();
+  const month = (dateObject.getMonth() + 1).toString().padStart(2, "0");
+  const day = dateObject.getDate().toString().padStart(2, "0");
+  const customDate = `${day}-${month}-${year}`;
+  console.log(customDate);
+  return customDate;
+}
+
+CodeforcesData();
+async function CodeforcesData() {
+  let url = "https://codeforces.com/api/user.rating?handle=a_r_y_a_n_anand";
+  let response = await fetch(url);
+  let kaamKeCheez = await response.json();
+
+  let dot = ["Start"];
+  let contestRating = [0];
+  let len = await kaamKeCheez.result.length;
+  for (let i = 0; i < len; i++)
+    contestRating.push(kaamKeCheez.result[i].newRating);
+  for (let i = 0; i < len; i++)
+    dot.push(TimeFormatter(kaamKeCheez.result[i].ratingUpdateTimeSeconds));
+
+  let data = {
+    labels: dot,
+    datasets: [
+      {
+        label: "Rating",
+        data: contestRating,
+        // --- Line Specific Styling ---
+        borderColor: "#0077b6", // Color of the line
+        backgroundColor: "#00b4d8", // Color under the line (optional)
+        tension: 0.25, // Smoothness of the line (0 is sharp, 0.4 is smooth)
+        fill: true, // Fills the area under the line
+        pointRadius: 5, // Size of the data points
+        pointHoverRadius: 9, // Size of data points on hover
+      },
+    ],
+  };
+
+  new Chart(ctx, {
+    type: "line",
+    data: data,
+    options: {
+      // Optional configuration options
+      responsive: true,
+      plugins: {
+        legend: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: "Codeforces Rating Curve",
+        },
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+          title: {
+            display: false,
+          },
+        },
+      },
+    },
+  });
+}
