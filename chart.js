fb57050a6ffb5bda5c94c8e04fd9e239cdf0d2f9
Aryan Anand <aryanaa2005@gmail.com>
2025-10-16 00:39:05 +0530

    codechef chart added

--- chart.js
+++ chart.js
@@ -1,5 +1,4 @@
-const ctx = document.getElementById("myLineChart");
-
+const cf = document.getElementById("codeforcesChart");
 function TimeFormatter(contestTimestampSeconds) {
   const contestTimestampMilliseconds = contestTimestampSeconds * 1000;
   const dateObject = new Date(contestTimestampMilliseconds);
@@ -11,14 +10,8 @@ function TimeFormatter(contestTimestampSeconds) {
   return customDate;
 }
 
-// Example usage (assuming an arbitrary timestamp):
-// For a timestamp corresponding to 2025-09-24
-// (the date you mentioned in your profile, which is a neat coincidence!)
-// let exampleTimestamp = 1758787200; // Sept 24, 2025 in seconds
-// TimeFormatter(exampleTimestamp); // Output: "09-25"
-
-CodeforcesData();
-async function CodeforcesData() {
+CodeforcesChart();
+async function CodeforcesChart() {
   let url = "https://codeforces.com/api/user.rating?handle=a_r_y_a_n_anand";
   let response = await fetch(url);
   let kaamKeCheez = await response.json();
@@ -48,7 +41,67 @@ async function CodeforcesData() {
     ],
   };
 
-  new Chart(ctx, {
+  new Chart(cf, {
+    type: "line",
+    data: data,
+    options: {
+      // Optional configuration options
+      responsive: true,
+      plugins: {
+        legend: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: "Codeforces Rating Curve",
+        },
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+          title: {
+            display: false,
+          },
+        },
+      },
+    },
+  });
+}
+
+const cc = document.getElementById("codechefChart");
+CodechefChart();
+function CodechefChart() {
+  let dot = [
+    "Start",
+    "08-25",
+    "08-25",
+    "08-25",
+    "08-25",
+    "09-25",
+    "09-25",
+    "09-25",
+    "10-25",
+  ];
+  let contestRating = [0, 916, 994, 1046, 1071, 1136, 1203, 1233, 1282];
+
+  let data = {
+    labels: dot,
+    datasets: [
+      {
+        label: "Rating",
+        data: contestRating,
+        // --- Line Specific Styling ---
+        borderColor: "#0077b6", // Color of the line
+        backgroundColor: "#00b4d8", // Color under the line (optional)
+        tension: 0.25, // Smoothness of the line (0 is sharp, 0.4 is smooth)
+        fill: true, // Fills the area under the line
+        pointRadius: 5, // Size of the data points
+        pointHoverRadius: 9, // Size of data points on hover
+      },
+    ],
+  };
+
+  new Chart(cc, {
     type: "line",
     data: data,
     options: {
