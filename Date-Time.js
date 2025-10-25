21399f314dd05ea9153d1adf3ee33ad7e23b0b8d
Aryan Anand <aryanaa2005@gmail.com>
2025-10-25 11:51:54 +0530

    current date and time added in the bottom

---
+++ Date-Time.js
@@ -0,0 +1,36 @@
+// Time();
+function Time() {
+  let now = new Date();
+  let hours = now.getHours();
+  let AMPM = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12; // Convert to 12-hour format
+  let minutes = now.getMinutes();
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  let day = now.getDate();
+  let month = now.getMonth();
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let year = now.getFullYear();
+  Display(hours, minutes, AMPM, day, monthNames[month], year);
+}
+
+setInterval(() => {
+  Time();
+}, 1000);
+
+async function Display(hour, minute, AMPM, day, month, year) {
+  let time = document.getElementById("time");
+  time.innerHTML = `${hour}:${minute} ${AMPM} on ${day} - ${month}, ${year}`;
+}
