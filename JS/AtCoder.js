3a83103060b8d437ecffc95ee669bdd789b679a8
Aryan Anand <aryanaa2005@gmail.com>
2026-01-27 13:30:23 +0530

    Atcoder data is added

---
+++ JS/AtCoder.js
@@ -0,0 +1,13 @@
+const atCoderList = document.getElementById("atCoder-list");
+
+let newItem = document.createElement("li");
+newItem.innerHTML = "Gray Coder";
+atCoderList.appendChild(newItem);
+
+newItem = document.createElement("li");
+newItem.textContent = "Current Rating  : 67 (12 Kyu)";
+atCoderList.appendChild(newItem);
+
+newItem = document.createElement("li");
+newItem.textContent = "Rated Matches  : 4";
+atCoderList.appendChild(newItem);
